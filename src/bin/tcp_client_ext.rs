0d670c67d1535c2694d3e30d9c683a6b5d17ea6c
yangzhang214024 <yangzhang214024@sohu-inc.com>
2020-05-21 18:31:48 +0800

    improve code

--- src/bin/tcp_client_ext.rs
+++ src/bin/tcp_client_ext.rs
@@ -4,48 +4,45 @@ use std::str::from_utf8;
 
 fn main() {
     let mut buffer = String::new();
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
-
-    handle.read_line(&mut buffer).unwrap();
-
-    // buffer.len() - trim_buffer.len() = 2, why? '\r\n' maybe?
-    let mut trim_buffer = buffer.trim_end();
-
-    println!("input {}", &trim_buffer);
-
-    let send_buffer = trim_buffer.as_bytes();
-
-    let length = send_buffer.len() as u32;
-
-    let size_header = length.to_le_bytes();
-    println!("size_header len {}", size_header.len());
-
-    println!("before match");
+    let mut trim_buffer="";
 
     match TcpStream::connect("localhost:5555") {
         Ok(mut stream) => {
             println!("Successfully connected to server");
 
-            stream.write(&size_header).unwrap();
-            stream.write(send_buffer).unwrap();
+            // loop read stdin and send input to server
+            while match trim_buffer {
+                "stop" => false, // type stop to exit client
+                _ => {
+                    buffer.clear();
+                    true
+                }
+            }
+            {
+                // get input without \n or \r\n
+                let stdin = io::stdin();
+                let mut handle = stdin.lock();
+                handle.read_line(&mut buffer).unwrap();
+                trim_buffer = buffer.trim_end();
 
-            println!("data sent");
+                println!("input {}", &trim_buffer);
 
-            // let mut data = [0 as u8; 6]; // using 6 byte buffer
-            // match stream.read_exact(&mut data) {
-            //     Ok(_) => {
-            //         if &data == msg {
-            //             println!("Reply is ok!");
-            //         } else {
-            //             let text = from_utf8(&data).unwrap();
-            //             println!("Unexpected reply: {}", text);
-            //         }
-            //     },
-            //     Err(e) => {
-            //         println!("Failed to receive data: {}", e);
-            //     }
-            // }
+                // string in bytes
+                let send_buffer = trim_buffer.as_bytes();
+
+                // len return usize witch is 64 bit in my project
+                // I only need u32 hence as u32
+                let length = send_buffer.len() as u32;
+
+                // get bytes for u32, socket would translate this to 'be', so keep it here in 'le'
+                let size_header = length.to_le_bytes();
+
+                // call me lazy for not use another buffer to put things together
+                stream.write(&size_header).unwrap();
+                stream.write(send_buffer).unwrap();
+
+                // println!("data sent");
+            }
         },
         Err(e) => {
             println!("Failed to connect: {}", e);
--- src/bin/tcp_server_ext.rs
+++ src/bin/tcp_server_ext.rs
@@ -4,7 +4,7 @@ use std::io::{Read, Write};
 use std::convert::TryInto;
 use std::str::from_utf8;
 
-fn read_le_u32_simple(input:  &[u8]) -> u32 {
+fn read_le_u32_simple(input: &[u8]) -> u32 {
     let (int_bytes, rest) = input.split_at(std::mem::size_of::<u32>());
     u32::from_le_bytes(int_bytes.try_into().unwrap())
 }
@@ -15,51 +15,55 @@ fn read_le_u32(input: &mut &[u8]) -> u32 {
     u32::from_le_bytes(int_bytes.try_into().unwrap())
 }
 
+// todo: implement a struct Frame for buffer, header as so on
+
 fn handle_client(mut stream: TcpStream){
     let mut header_leanth: usize = 4;
     let mut body_size = 0;
+    // buffer for incoming frame
     let mut data = Vec::<u8>::new();
 
-    println!("data len {}", &data.len());
-
-
-    let mut buffer = [0 as u8; 50]; // using 50 byte buffer
+     // using 50 byte as read buffer
+    let mut buffer = [0 as u8; 50];
 
     while match stream.read(&mut buffer) {
         Ok(size) => match size {
             0 => {
+                // connection close
                 println!("size 0, close");
                 false
             },
             _ => {
-                // echo everything!
-                // stream.write(&buffer[0..size]).unwrap();
-                println!("size {}", &size);
-                println!("buffer {:?}", buffer.clone().to_vec());
-                println!("buffer len {}", buffer.len());
+                // println!("size {}", &size);
+                // println!("buffer {:?}", buffer.clone().to_vec());
+                // println!("buffer len {}", buffer.len());
 
+                // write buffer to frame (data buffer)
                 data.extend(&buffer[0..size]);
-                // data[buf_pos..].copy_from_slice(&buffer[0..size]);
 
-                if data.len() > header_leanth {
+                if data.len() > header_leanth { // make sure we have enough data for header
 
+                    // read body size from header
                     if  body_size < 1 {
+
                         let size_header = &data[0..5];
                         body_size = read_le_u32_simple(&size_header) as usize;
 
                         println!("body size {}", body_size);
                     }
 
+                    // current body length
                     let body_length = data.len() - header_leanth;
 
-                    if  body_length>= body_size {
-                        // one frame finished
+                    // one frame finished, maybe next frame appended
+                    if body_length>= body_size {
 
+                        // get text out of body
                         let body = &data[header_leanth..body_size+header_leanth];
                         let text = from_utf8(&body).unwrap();
                         println!("received data: {}", &text);
 
-
+                        // move data from next frame to the head of buffer
                         let next_frame_len = data.len() - body_size - header_leanth;
                         if next_frame_len > 0 {
                             println!("next frame data.len {}, body size {}, diff {}", data.len(), body_size, next_frame_len);
@@ -67,9 +71,11 @@ fn handle_client(mut stream: TcpStream){
                             left[0..next_frame_len].copy_from_slice(&right);
                         }
 
+                        // set all 'pointers' in right place
                         data.truncate(next_frame_len);
                         data.shrink_to_fit();
 
+                        // prepare for next frame
                         body_size=0;
                     }
                 }
